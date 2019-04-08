f41a134e1cd02083f97a83e35bd915634e33856c
Evgeni Kolev <ekoleww@gmail.com>
2019-04-08 20:10:20 +0300

    working buttons

--- main.js
+++ main.js
@@ -1,4 +1,67 @@
- 
-
- var editor = edit.document;
+ const editor = edit.document;
  editor.designMode = "on";
+
+ const styleText = document.querySelector('#style-text');
+
+ // style the fonts
+ const fonts = document.querySelectorAll('select.font-change > option');
+    for (let i = 0; i < fonts.length; i++){
+        fonts[i].style.fontFamily = fonts[i].value;
+}
+
+
+//constants of buttons
+const Bold = document.querySelector('.bold');
+const Italic = document.querySelector('.italic');
+const unorderedList = document.querySelector('.unordered-list');
+const orderedList = document.querySelector('.ordered-list');
+const alignLeft = document.querySelector('.align-left');
+const alignCenter = document.querySelector('.align-center');
+const alignRight = document.querySelector('.align-right');
+
+Bold.addEventListener('click', function(){
+    editor.execCommand('Bold', false,null);
+    changeColor(Bold);
+});
+
+Italic.addEventListener('click', function(){
+    editor.execCommand('Italic', false,null);
+    changeColor(Italic);
+});
+
+unorderedList.addEventListener('click',function(){
+    editor.execCommand('InsertUnorderedList', false,"newOL");
+    changeColor(unorderedList);
+});
+
+
+orderedList.addEventListener('click',function(){
+    editor.execCommand('InsertOrderedList', false,"newOL" + Math.round(Math.random() * 1000));
+    changeColor(orderedList);
+});
+
+alignLeft.addEventListener('click',function(){
+    editor.execCommand('justifyLeft', false, null);
+    changeColor(alignLeft);
+});
+
+alignCenter.addEventListener('click',function(){
+    editor.execCommand('justifyCenter', false, null);
+    changeColor(alignCenter);
+});
+
+alignRight.addEventListener('click',function(){
+    editor.execCommand('justifyRight', false, null);
+    changeColor(alignRight);
+});
+
+
+// change color of clicked buttons
+function changeColor(e){
+
+    if (e.classList.contains('clicked')){
+        e.classList.remove('clicked');
+    }else{
+   e.classList.add('clicked');
+    }
+}
