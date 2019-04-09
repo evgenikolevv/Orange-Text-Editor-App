eddcd08110b14d93bc168486f42c5c8d88a0aa80
Evgeni Kolev <ekoleww@gmail.com>
2019-04-09 20:09:45 +0300

    simple design
    new buttons

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
  var editor = edit.document;
  editor.designMode = "on";
 
+
+
  
  const styleText = document.querySelector('#style-text');
+
  // style the fonts
  const fonts = document.querySelectorAll('select.font-change > option');
     for (let i = 0; i < fonts.length; i++){
@@ -19,6 +22,8 @@ const orderedList = document.querySelector('.ordered-list');
 const alignLeft = document.querySelector('.align-left');
 const alignCenter = document.querySelector('.align-center');
 const alignRight = document.querySelector('.align-right');
+const link = document.querySelector('.link');
+const insertImage = document.querySelector('.image');
 const fontColor = document.querySelector('.font-color');
 const fontChange = document.querySelector('.font-change');
 const fontSize = document.querySelector('.font-size');
@@ -64,6 +69,19 @@ alignRight.addEventListener('click',function(){
     changeColor(alignRight);
 });
 
+link.addEventListener('click', function(){
+    let url = prompt("Enter a URL", "http://");
+    editor.execCommand('CreateLink', false, url);
+    changeColor(link);
+});
+
+insertImage.addEventListener('click', function(){
+    let url = prompt("Enter a URL", "http://");
+    editor.execCommand('InsertHTML', prompt('Insert Image', false, url));
+    changeColor(insertImage);
+
+});
+
 fontColor.addEventListener('change', function(e){
     editor.execCommand('ForeColor', false, e.target.value);
 });
@@ -81,10 +99,16 @@ fontSize.addEventListener('change',function(e){
 function changeColor(e){
 
     if (e.classList.contains('clicked')){
+        
         e.classList.remove('clicked');
+        e.style.animation = `click 0.5s ease-in`;
     }else{
    e.classList.add('clicked');
+   e.style.animation = `click 0.5s ease`;
+
     }
+
+  
 }
 
 
